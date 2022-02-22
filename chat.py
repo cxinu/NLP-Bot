17388d982e02ae373e87d21a19f0ce7ecc667f9d
cxinu <cxinu3099@gmail.com>
2022-02-22 19:08:54 +0530

    Add files via upload

--- chat.py
+++ chat.py
@@ -1,5 +1,6 @@
 import random
 import json
+from unicodedata import name
 
 import torch
 
@@ -27,12 +28,9 @@ model.eval()
 
 bot_name = "Sam"
 print("Let's chat! (type 'quit' to exit)")
-while True:
-    # sentence = "do you use credit cards?"
-    sentence = input("You: ")
-    if sentence == "quit":
-        break
 
+
+def response(sentence) -> str:
     sentence = tokenize(sentence)
     X = bag_of_words(sentence, all_words)
     X = X.reshape(1, X.shape[0])
@@ -48,6 +46,6 @@ while True:
     if prob.item() > 0.75:
         for intent in intents['intents']:
             if tag == intent["tag"]:
-                print(f"{bot_name}: {random.choice(intent['responses'])}")
+                return random.choice(intent['responses'])
     else:
-        print(f"{bot_name}: I do not understand...")
+        return "I do not understand..."
